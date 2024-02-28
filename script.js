f47419b7af2f9bb6802712837f5382c8fa7a1ec5
TejasChavan7 <tejas007chavan@gmail.com>
2024-02-28 23:49:47 +0530

    complete

---
+++ script.js
@@ -0,0 +1,37 @@
+var t1 = gsap.timeline()
+t1.from("#nav h3",{
+
+    y:-50,
+    opacity:0,
+    deley:0.4,
+    duration:1,
+    stagger:0.3
+})
+
+t1.from("#main h1",{
+     
+    x:-500,
+    opacity:0,
+    duration:0.8,
+    stagger:0.4
+})
+
+t1.from("img",{
+     
+    x:100,
+    rotate:45,
+    opacity:0,
+    duration:0.5,
+    stagger:0.5
+
+
+})
+
+t1.from("#bot h3",{
+
+    y:-50,
+    opacity:0,
+    deley:0.4,
+    duration:1,
+    stagger:0.2
+})
